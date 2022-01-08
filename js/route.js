fd37e0ba1cd735d9cd687e6f0adac1d400d30676
builder.fun4cheats.com <ykaanbayat@gmail.com>
2022-01-08 14:27:53 +0300

    hot fix

--- js/route.js
+++ js/route.js
@@ -2,22 +2,32 @@ const binance = require('./binance.js');
 const telegram_bot = require('./telegram.js');
 
 module.exports = function (app) {
-
+    let buy_state = false;
     app.post( '/api/binance_trade', async function ( req,res ) {
-        const { coin, action, price } = req.body;
-        price = binance.get_current_price( coin ); // get price from binance cuz have diff.
+        const { action } = req.body;
+        let price = await binance.get_current_price( 'BTCUSDT' ); // get price from binance cuz have diff.
+        //var usdt_balance = await binance.get_balance('USDT');
         if ( action == 'VERIFY' ) {
-            binance.set_state( true );
-        } else if( action === 'BUY' && binance.get_state() ) {
-            let amount = await binance.calculate_buy_amount( 'USDT', price );
-            binance.buy_coin( coin, amount );
-            telegram_bot.send_message( `Bought ${coin} with ${amount} for ${price}` );
-            binance.set_state( false );
-        } else if( action === 'SELL' && binance.get_state() ) {
-            let amount = await binance.get_coin_amount( 'BTC', price );
-            binance.sell_coin( coin, amount );
-            telegram_bot.send_message( `Sold ${amount}x ${coin} for ${price}` );
-            binance.set_state( false );
+            if( buy_state ) {
+                buy_state = false;
+            } else {
+                buy_state = true;
+            }
+        }
+        if( buy_state ) {
+            //if ( price > 0.00020  ) {
+                let amount = await binance.calculate_buy_amount( 'USDT', price );
+                binance.buy_coin( 'BTCUSDT', amount );
+                telegram_bot.send_message( `Bought 'BTCUSDT' with ${amount} for ${price}` );
+                binance.set_state( false );
+            //}
+        } else{
+            if ( price > 0.00020 ) {
+                let amount = await binance.get_coin_amount( 'BTCUSDT' );
+                binance.sell_coin( 'BTCUSDT', amount );
+                telegram_bot.send_message( `Sold 'BTCUSDT' with ${amount} for ${price}` );
+                binance.set_state( false );
+            }
         }
 
         res.sendStatus( 200 );
--- js/telegram.js
+++ js/telegram.js
@@ -6,18 +6,18 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
 bot.on('message', async (msg) => {
     let price = await binance.get_current_price( 'BTCUSDT' );
-
+    console.log( msg.chat.id );
     if ( msg.chat.id == process.env.SWZ_ID ) {
         if ( msg.text == "buy" ) {
             let amount = await binance.calculate_buy_amount( 'USDT', price );
             binance.buy_coin( 'BTCUSDT', amount );
-            send_message( `Bought BTCUSDT with ${amount} for ${price}` );
+            send_message( `Bought BTCUSDT with ${amount} for ${price}`, msg.chat.id );
             binance.set_state( false );
         }
         if ( msg.text == "sell" ) {
             let amount = await binance.get_coin_amount( 'BTC', price );
             binance.sell_coin( 'BTCUSDT', amount );
-            send_message( `Sold ${amount}x BTCUSDT for ${price}` );
+            send_message( `Sold ${amount}x BTCUSDT for ${price}`, msg.chat.id );
             binance.set_state( false );
         }
     }
@@ -34,8 +34,8 @@ bot.on('message', async (msg) => {
 
 });
 
-function send_message( message ) {
-    bot.sendMessage( process.env.SWZ_ID, message );
+function send_message( message, id ) {
+    bot.sendMessage( id ? id : process.env.GROUP_ID, message );
 }
 
 module.exports = {
