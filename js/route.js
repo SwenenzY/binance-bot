fb871556e74dabebd1ecbcd468d85611a18e37bf
builder.fun4cheats.com <ykaanbayat@gmail.com>
2022-01-26 23:12:13 +0300

    atom

--- js/route.js
+++ js/route.js
@@ -2,13 +2,13 @@ const binance = require('./binance.js');
 const telegram_bot = require('./telegram.js');
 
 let usdt_price = 0; // % change
-let last_price = 0; // last btc price
+let last_price = 0; // last ATOM price
 
 module.exports = function (app) {
     let buy_state = false;
     app.post( '/api/binance_trade', async function ( req,res ) {
         const { action } = req.body;
-        let price = await binance.get_current_price( 'BTCUSDT' ); // get price from binance cuz have diff.
+        let price = await binance.get_current_price( 'ATOMUSDT' ); // get price from binance cuz have diff.
         var usdt_balance = await binance.get_balance('USDT');
         if ( action == 'VERIFY' ) {
             binance.set_state( !binance.get_state( ) );
@@ -19,18 +19,18 @@ module.exports = function (app) {
             usdt_price = usdt_balance;
 
             let amount = await binance.calculate_buy_amount( 'USDT', price );
-            binance.buy_coin( 'BTCUSDT', amount );
-            telegram_bot.send_message( `Bought 'BTCUSDT' with ${amount} for ${price}` );
+            binance.buy_coin( 'ATOMUSDT', amount );
+            telegram_bot.send_message( `Bought 'ATOMUSDT' with ${amount} for ${price}` );
 
         } else {
-            if ( price > 0.00020 ) { // btc price bigger than 0.00020
-                // sell btc
-                let amount = await binance.get_coin_amount( 'BTC' ); // get amount of btc
-                binance.sell_coin( 'BTCUSDT', amount ); // sell btc
+            if ( price > 0.00020 ) { // ATOM price bigger than 0.00020
+                // sell ATOM
+                let amount = await binance.get_coin_amount( 'ATOM' ); // get amount of ATOM
+                binance.sell_coin( 'ATOMUSDT', amount ); // sell ATOM
                 // get usdt balance
                 var usdt_balance = await binance.get_balance('USDT');
                 let percent = binance.perc_increase( usdt_price, usdt_balance ); // get % change
-                telegram_bot.send_message( `${last_price < price ? 'WIN' : 'LOSS'} (%${percent}) Sold 'BTCUSDT' with ${amount} for ${price}` );
+                telegram_bot.send_message( `${last_price < price ? 'WIN' : 'LOSS'} (%${percent}) Sold 'ATOMUSDT' with ${amount} for ${price}` );
 
                 // reset prices
                 last_price = 0;
--- js/telegram.js
+++ js/telegram.js
@@ -5,30 +5,30 @@ const env = require('dotenv').config();
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
 bot.on('message', async (msg) => {
-    let price = await binance.get_current_price( 'BTCUSDT' );
+    let price = await binance.get_current_price( 'ATOMUSDT' );
     console.log( msg.chat.id );
     if ( msg.chat.id == process.env.SWZ_ID ) {
         if ( msg.text == "buy" ) {
             let amount = await binance.calculate_buy_amount( 'USDT', price );
-            binance.buy_coin( 'BTCUSDT', amount );
-            send_message( `Bought BTCUSDT with ${amount} for ${price}`, msg.chat.id );
+            binance.buy_coin( 'ATOMUSDT', amount );
+            send_message( `Bought ATOMUSDT with ${amount} for ${price}`, msg.chat.id );
             binance.set_state( false );
         }
         if ( msg.text == "sell" ) {
-            let amount = await binance.get_coin_amount( 'BTC', price );
-            binance.sell_coin( 'BTCUSDT', amount );
-            send_message( `Sold ${amount}x BTCUSDT for ${price}`, msg.chat.id );
+            let amount = await binance.get_coin_amount( 'ATOM', price );
+            binance.sell_coin( 'ATOMUSDT', amount );
+            send_message( `Sold ${amount}x ATOMUSDT for ${price}`, msg.chat.id );
             binance.set_state( false );
         }
     }
     if ( msg.text == "price" ) {
-        bot.sendMessage( msg.chat.id, `BTCUSDT: ${price}` );
+        bot.sendMessage( msg.chat.id, `ATOMUSDT: ${price}` );
     }
 
     if( msg.text == "balance" ) {
         await binance.get_balances().then( balances => {
             bot.sendMessage( msg.chat.id, `USDT: ${balances.USDT.available}` );
-            bot.sendMessage( msg.chat.id, `BTC: ${balances.BTC.available}` );
+            bot.sendMessage( msg.chat.id, `ATOM: ${balances.ATOM.available}` );
         } );
     }
 
